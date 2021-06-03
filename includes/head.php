12dd5a1bc7e48be0b5cb71210ed0343b946147ce
11guptakhushi <71972602+11guptakhushi@users.noreply.github.com>
2021-06-03 19:29:16 +0530

    Add files via upload

--- includes/head.php
+++ includes/head.php
@@ -3,28 +3,68 @@
 	ob_start();
 	include("dbconnect.php");
 ?>	
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
 <head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<script src="https://kit.fontawesome.com/a076d05399.js"></script>
 <title>Food Ordering System</title>
 <style>
 body
 {
 	margin:0;
-	width:100%;
 }
- .menu
-{
-	width: 100%;
-	height:62px;
-	background-color:white;	
-	font-family:sans-serif;
-	font-size:14px;
-	z-index:500;
+.menu{
+	background-color:white;
+}
+.sidebar{
+	background-color: #de1302;
+	max-width:400px;
+	width:100%;
+	height:100vh;
+	
+	position:fixed;
+	top:0px;
+	right:0;
+	
+	display:flex;
+	flex-direction: column;
+	align-items:center;
+	justify-content:center;
+	
+	color:white;
+	transform: translateX(100%);
+	transition:transform 0.24s ease-in;
+	
+	z-index:1;
+}
+.sidebar.active{
+	transform: translateX(0%);
+}
+.close{
+	position:absolute;
+	top:30px;
+	right:40px;
+	font-size: 30px;
+	color: white;
+	float:right;
+	line-height: 80px;
+	margin:20px 40px;
+	cursor: pointer;
+}
+
+.menu .button{
+	font-size: 30px;
+	color: black;
+	float:right;
+	line-height: 80px;
+	margin:20px 40px;
+	cursor: pointer;
 }
  #logo
 {
 	width:300px;
 	height:62px;
-	float:left;
 }
  .menu ul
 {
@@ -33,61 +73,54 @@ body
 	padding: 0px;
 	position: relative;
 	float: right;		
+	font-family:sans-serif;
+	font-size:16px;
+	position:relative;
+	z-index:500;
+	font-weight:600;	
 }
  .menu ul li
 {
 	text-align:center;	
-	width: 180px;
+	width: 100%;
 	height:50px;
 	line-height: 62px;
 	letter-spacing:1px;
 	display:inline-block;
-	font-weight:bold;	
 }
  .menu ul li a
 {
+	color:white;
 	text-decoration:none;
 	display:block;
-	color:black;
 }
- .menu ul li a:hover 
+ .menu ul li a.active, a:hover 
 {
-	border-bottom:3px solid #DE1302;
+	color:white;
+	border-bottom:3px solid white;
 	font-weight:bold;
-	color:#DE1302;
-}
- .menu ul ul
-{
-	display:none;
-}
- .menu ul li:hover ul
-{	
-	display:block;
-	margin-top: 0px;
-	border:1px solid;	
-	border-top:1px outset #DE1302;
-	z-index:500;
-}
- .menu ul li:hover ul li
-{
-	display:block;
-	background:white;
-}
- .menu button
-{
-	border:none;
-	background:none;
-	width: 180px;
-	height:50px;
-	cursor:pointer;
 }
 #profile
 {
 	vertical-align:middle; 
 	border-radius:50%;
 }
+ .menu button
+{
+	border:none;
+	background:none;
+	width: 100%;
+	height:50px;
+	cursor:pointer;
+	font-size:16px;
+	color:white;	
+}
+.menu button:hover{
+	border-bottom:3px solid white;
+}
 </style>
 </head>
+
 <body>
 <?php
 if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) 
@@ -111,19 +144,24 @@ if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true)
 	{
 		echo "
 		<div class='menu'>
-			<img src='images/logo/logo.jpg'/>
-			<ul>
-				<li><a href='homepage.php'>Home</a></li>
-				<li><a href='about.php'>About Us</a></li>
-				<li><a href='contact_us.php'>Contact Us</a></li>		
-				<li><a><img src='images/profile.jpg' width='30px' height='30px' id='profile'> ".$user["Username"]."</a>
+			<div class='button' onclick='toggleSidebar()'>
+				<i class='fas fa-bars'></i>
+			</div>
+			<div class='sidebar'>
+				<div class='close' onclick='toggleSidebar()'>
+					<i class='fas fa-close'></i>
+				</div>
+				<div>
 					<ul>
-						<li><a href=''>My Account</a></li>
-						<li><a href=''>My Order</a></li>
+						<li style='color:black'>Hi, ".$user["Username"]."</li>
+						<li><a href='homepage.php'>Home</a></li>					
+						<li><a href='about.php'>About Us</a></li>
+						<li><a href='contact_us.php'>Contact Us</a></li>
 						<li><form method='get'><button type='submit' name='logout' style='font-weight:bold;'>Log Out</button></form></li>
-					</ul>
-				</li>
-			</ul>
+					</ul>	
+				</div>
+			</div>
+			<img src='images/logo/logo.jpg'/>
 		</div>";
 	}
 }
@@ -131,17 +169,26 @@ else
 {
 	echo "
 	<div class='menu'>
+		<div class='button' onclick='toggleSidebar()'>
+			<i class='fas fa-bars'></i>
+		</div>
+		<div class='sidebar'>
+			<div class='close' onclick='toggleSidebar()'>
+				<i class='fas fa-close'></i>
+			</div>
+			<div>
+				<ul>
+					<li><a href='homepage.php'>Home</a></li>
+					<li><a href='login.php'>Login</a></li>
+					<li><a href='registration.php' >Create an Account</a></li>
+					<li><a href='about.php'>About Us</a></li>
+					<li><a href='contact_us.php'>Contact Us</a></li>
+				</ul>	
+			</div>
+		</div>
 		<img src='images/logo/logo.jpg'/>
-		<ul>
-			<li><a href='homepage.php'>Home</a></li>
-			<li><a href='login.php'>Login</a></li>
-			<li><a href='registration.php' >Create an Account</a></li>
-			<li><a href='about.php'>About Us</a></li>
-			<li><a href='contact_us.php'>Contact Us</a></li>
-		</ul>
 	</div>";		
 }
-
 if (isset($_GET['logout'])) 
 {							
 		session_destroy();
@@ -151,3 +198,9 @@ if (isset($_GET['logout']))
 ?>
 </body>
 </html>
+<script>
+	function toggleSidebar(){
+		const sidebar = document.querySelector(".sidebar");
+		sidebar.classList.toggle("active");
+	}
+</script>
